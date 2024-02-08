9b35cbb6d6321fb23f6dc386ef60f5587a3b39bf
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-02-08 13:06:20 +0530

    ref: renamed email regex variable

--- internal/pkg/constants/app.go
+++ internal/pkg/constants/app.go
@@ -2,7 +2,7 @@ package constants
 
 const (
 	SERVER_ADDRESS = "127.0.0.1:8080"
-	REGEX          = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$"
+	EMAIL_REGEX    = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$"
 )
 
 const (
