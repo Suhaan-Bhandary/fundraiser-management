5a8eb3e353771bd0504fc8cfc81db7da5d8250bb
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-02-08 11:02:43 +0530

    feat: tested user login

--- internal/app/user/service_test.go
+++ internal/app/user/service_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/Suhaan-Bhandary/fundraiser-management/internal/pkg/dto"
+	"github.com/Suhaan-Bhandary/fundraiser-management/internal/pkg/helpers"
 	"github.com/Suhaan-Bhandary/fundraiser-management/internal/repository/mocks"
 	"github.com/stretchr/testify/mock"
 )
@@ -25,7 +26,7 @@ func TestRegisterUser(t *testing.T) {
 				FirstName: "suhaan",
 				LastName:  "bhandary",
 				Email:     "suhaanbhandary1@gmail.com",
-				Password:  "hi",
+				Password:  "123",
 			},
 			setup: func(userMock *mocks.UserStorer) {
 				userMock.On("RegisterUser", mock.Anything).Return(nil).Once()
@@ -38,7 +39,7 @@ func TestRegisterUser(t *testing.T) {
 				FirstName: "suhaan",
 				LastName:  "bhandary",
 				Email:     "suhaanbhandary1@gmail.com",
-				Password:  "hi",
+				Password:  "123",
 			},
 			setup: func(userMock *mocks.UserStorer) {
 				userMock.On("RegisterUser", mock.Anything).Return(errors.New("Error")).Once()
@@ -53,6 +54,67 @@ func TestRegisterUser(t *testing.T) {
 
 			// test service
 			err := service.RegisterUser(test.input)
+
+			if (err != nil) != test.isErrorExpected {
+				t.Errorf("Test Failed, expected error to be %v, but got err %v", test.isErrorExpected, err != nil)
+			}
+		})
+	}
+}
+
+func TestLoginUser(t *testing.T) {
+	userRepo := mocks.NewUserStorer(t)
+	service := NewService(userRepo)
+
+	tests := []struct {
+		name            string
+		input           dto.LoginUserRequest
+		setup           func(userMock *mocks.UserStorer, password string)
+		isErrorExpected bool
+	}{
+		{
+			name: "Success for User Login",
+			input: dto.LoginUserRequest{
+				Email:    "suhaanbhandary1@gmail.com",
+				Password: "123",
+			},
+			setup: func(userMock *mocks.UserStorer, password string) {
+				hashedPassword, _ := helpers.HashPassword(password)
+				userMock.On("GetUserIDPassword", mock.Anything).Return(0, hashedPassword, nil).Once()
+			},
+			isErrorExpected: false,
+		},
+		{
+			name: "Failed because invalid email",
+			input: dto.LoginUserRequest{
+				Email:    "abc@gmail.com",
+				Password: "123",
+			},
+			setup: func(userMock *mocks.UserStorer, _ string) {
+				userMock.On("GetUserIDPassword", mock.Anything).Return(-1, "", errors.New("Error")).Once()
+			},
+			isErrorExpected: true,
+		},
+		{
+			name: "Failed because invalid password",
+			input: dto.LoginUserRequest{
+				Email:    "abc@gmail.com",
+				Password: "123",
+			},
+			setup: func(userMock *mocks.UserStorer, _ string) {
+				hashedPassword, _ := helpers.HashPassword("other")
+				userMock.On("GetUserIDPassword", mock.Anything).Return(0, hashedPassword, nil).Once()
+			},
+			isErrorExpected: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			test.setup(userRepo, test.input.Password)
+
+			// test service
+			_, err := service.LoginUser(test.input)
 
 			if (err != nil) != test.isErrorExpected {
 				t.Errorf("Test Failed, expected error to be %v, but got err %v", test.isErrorExpected, err != nil)
