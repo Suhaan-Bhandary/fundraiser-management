1aa97e91df638c099315500cb479c271ca31c8a4
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-02-13 13:23:38 +0530

    ref: refactored router

--- internal/api/router.go
+++ internal/api/router.go
@@ -15,6 +15,13 @@ func notFoundHandler(w http.ResponseWriter, _ *http.Request) {
 	middleware.ErrorResponse(w, 404, error)
 }
 
+var (
+	USER_ONLY            = []string{constants.USER}
+	ORGANIZER_ONLY       = []string{constants.ORGANIZER}
+	ADMIN_ONLY           = []string{constants.ADMIN}
+	ORGANIZER_ADMIN_ONLY = []string{constants.ORGANIZER, constants.ADMIN}
+)
+
 func NewRouter(deps app.Dependencies) *mux.Router {
 	router := mux.NewRouter()
 
@@ -22,19 +29,10 @@ func NewRouter(deps app.Dependencies) *mux.Router {
 	userRouter := router.PathPrefix("/user").Subrouter()
 	userRouter.HandleFunc("/register", RegisterUserHandler(deps.UserService)).Methods(http.MethodPost)
 	userRouter.HandleFunc("/login", LoginUserHandler(deps.UserService)).Methods(http.MethodPost)
-	userRouter.HandleFunc("", middleware.CheckAuth(ListUsersHandler(deps.UserService), []string{constants.ADMIN})).Methods(http.MethodGet)
-	userRouter.HandleFunc(
-		"/{id}",
-		middleware.CheckAuth(DeleteUserHandler(deps.UserService), []string{constants.ADMIN}),
-	).Methods(http.MethodDelete)
-	userRouter.HandleFunc(
-		"/profile",
-		middleware.CheckAuth(GetUserProfileHandler(deps.UserService), []string{constants.USER}),
-	).Methods(http.MethodGet)
-	userRouter.HandleFunc(
-		"/donation",
-		middleware.CheckAuth(ListUserDonationsHandler(deps.DonationService), []string{constants.USER}),
-	).Methods(http.MethodGet)
+	userRouter.HandleFunc("", middleware.CheckAuth(ListUsersHandler(deps.UserService), ADMIN_ONLY)).Methods(http.MethodGet)
+	userRouter.HandleFunc("/{id}", middleware.CheckAuth(DeleteUserHandler(deps.UserService), ADMIN_ONLY)).Methods(http.MethodDelete)
+	userRouter.HandleFunc("/profile", middleware.CheckAuth(GetUserProfileHandler(deps.UserService), USER_ONLY)).Methods(http.MethodGet)
+	userRouter.HandleFunc("/donation", middleware.CheckAuth(ListUserDonationsHandler(deps.DonationService), USER_ONLY)).Methods(http.MethodGet)
 
 	// Admin routes
 	adminRouter := router.PathPrefix("/admin").Subrouter()
@@ -44,62 +42,27 @@ func NewRouter(deps app.Dependencies) *mux.Router {
 	organizerRouter := router.PathPrefix("/organizer").Subrouter()
 	organizerRouter.HandleFunc("/register", RegisterOrganizerHandler(deps.OrganizerService)).Methods(http.MethodPost)
 	organizerRouter.HandleFunc("/login", LoginOrganizerHandler(deps.OrganizerService)).Methods(http.MethodPost)
-	organizerRouter.HandleFunc(
-		"",
-		middleware.CheckAuth(ListOrganizersHandler(deps.OrganizerService), []string{constants.ADMIN}),
-	).Methods(http.MethodGet)
-	organizerRouter.HandleFunc(
-		"",
-		middleware.CheckAuth(UpdateOrganizerHandler(deps.OrganizerService), []string{constants.ORGANIZER}),
-	).Methods(http.MethodPut)
-	organizerRouter.HandleFunc(
-		"/{id}",
-		GetOrganizerHandler(deps.OrganizerService),
-	).Methods(http.MethodGet)
-	organizerRouter.HandleFunc(
-		"/{id}",
-		middleware.CheckAuth(DeleteOrganizerHandler(deps.OrganizerService), []string{constants.ADMIN}),
-	).Methods(http.MethodDelete)
-	organizerRouter.HandleFunc(
-		"/{id}/verify",
-		middleware.CheckAuth(VerifyOrganizerHandler(deps.OrganizerService), []string{constants.ADMIN}),
-	).Methods(http.MethodPatch)
+	organizerRouter.HandleFunc("", middleware.CheckAuth(ListOrganizersHandler(deps.OrganizerService), ADMIN_ONLY)).Methods(http.MethodGet)
+	organizerRouter.HandleFunc("", middleware.CheckAuth(UpdateOrganizerHandler(deps.OrganizerService), ORGANIZER_ONLY)).Methods(http.MethodPut)
+	organizerRouter.HandleFunc("/{id}", GetOrganizerHandler(deps.OrganizerService)).Methods(http.MethodGet)
+	organizerRouter.HandleFunc("/{id}", middleware.CheckAuth(DeleteOrganizerHandler(deps.OrganizerService), ADMIN_ONLY)).Methods(http.MethodDelete)
+	organizerRouter.HandleFunc("/{id}/verify", middleware.CheckAuth(VerifyOrganizerHandler(deps.OrganizerService), ADMIN_ONLY)).Methods(http.MethodPatch)
 
 	// Fundraiser routes
 	fundraiserRouter := router.PathPrefix("/fundraiser").Subrouter()
-	fundraiserRouter.HandleFunc("",
-		middleware.CheckAuth(CreateFundraiserHandler(deps.FundraiserService), []string{constants.ORGANIZER}),
-	).Methods(http.MethodPost)
-	fundraiserRouter.HandleFunc("",
-		ListFundraisersHandler(deps.FundraiserService),
-	).Methods(http.MethodGet)
+	fundraiserRouter.HandleFunc("", middleware.CheckAuth(CreateFundraiserHandler(deps.FundraiserService), ORGANIZER_ONLY)).Methods(http.MethodPost)
+	fundraiserRouter.HandleFunc("", ListFundraisersHandler(deps.FundraiserService)).Methods(http.MethodGet)
 	fundraiserRouter.HandleFunc("/{id}", GetFundraiserHandler(deps.FundraiserService)).Methods(http.MethodGet)
-	fundraiserRouter.HandleFunc("/{id}",
-		middleware.CheckAuth(UpdateFundraiserHandler(deps.FundraiserService), []string{constants.ORGANIZER}),
-	).Methods(http.MethodPut)
-	fundraiserRouter.HandleFunc("/{id}",
-		middleware.CheckAuth(DeleteFundraiserHandler(deps.FundraiserService), []string{constants.ORGANIZER, constants.ADMIN}),
-	).Methods(http.MethodDelete)
-	fundraiserRouter.HandleFunc("/{id}/donation",
-		middleware.CheckAuth(CreateDonationHandler(deps.DonationService), []string{constants.USER}),
-	).Methods(http.MethodPost)
-	fundraiserRouter.HandleFunc("/{id}/donation",
-		ListFundraiserDonationsHandler(deps.DonationService),
-	).Methods(http.MethodGet)
-	fundraiserRouter.HandleFunc("/{id}/close",
-		middleware.CheckAuth(CloseFundraiserHandler(deps.FundraiserService), []string{constants.ORGANIZER}),
-	).Methods(http.MethodPatch)
-	fundraiserRouter.HandleFunc("/{id}/ban",
-		middleware.CheckAuth(BanFundraiserHandler(deps.FundraiserService), []string{constants.ADMIN}),
-	).Methods(http.MethodPatch)
-	fundraiserRouter.HandleFunc("/{id}/unban",
-		middleware.CheckAuth(UnBanFundraiserHandler(deps.FundraiserService), []string{constants.ADMIN}),
-	).Methods(http.MethodPatch)
+	fundraiserRouter.HandleFunc("/{id}", middleware.CheckAuth(UpdateFundraiserHandler(deps.FundraiserService), ORGANIZER_ONLY)).Methods(http.MethodPut)
+	fundraiserRouter.HandleFunc("/{id}", middleware.CheckAuth(DeleteFundraiserHandler(deps.FundraiserService), ORGANIZER_ADMIN_ONLY)).Methods(http.MethodDelete)
+	fundraiserRouter.HandleFunc("/{id}/donation", middleware.CheckAuth(CreateDonationHandler(deps.DonationService), USER_ONLY)).Methods(http.MethodPost)
+	fundraiserRouter.HandleFunc("/{id}/donation", ListFundraiserDonationsHandler(deps.DonationService)).Methods(http.MethodGet)
+	fundraiserRouter.HandleFunc("/{id}/close", middleware.CheckAuth(CloseFundraiserHandler(deps.FundraiserService), ORGANIZER_ONLY)).Methods(http.MethodPatch)
+	fundraiserRouter.HandleFunc("/{id}/ban", middleware.CheckAuth(BanFundraiserHandler(deps.FundraiserService), ADMIN_ONLY)).Methods(http.MethodPatch)
+	fundraiserRouter.HandleFunc("/{id}/unban", middleware.CheckAuth(UnBanFundraiserHandler(deps.FundraiserService), ADMIN_ONLY)).Methods(http.MethodPatch)
 
 	// Donation list
-	router.HandleFunc("/donation",
-		middleware.CheckAuth(ListDonationsHandler(deps.DonationService), []string{constants.ADMIN}),
-	).Methods(http.MethodGet)
+	router.HandleFunc("/donation", middleware.CheckAuth(ListDonationsHandler(deps.DonationService), ADMIN_ONLY)).Methods(http.MethodGet)
 
 	// Not Found Router
 	router.HandleFunc("/", notFoundHandler).Methods(http.MethodGet)
